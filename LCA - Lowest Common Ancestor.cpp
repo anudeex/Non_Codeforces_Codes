b74ea643a2b0ecd08ffc74f5b50902e5707e5284
Anudeex Shetty <anudeexshetty97@gmail.com>
2018-01-24 01:40:36 +0530

    Update LCA - Lowest Common Ancestor.cpp

--- LCA - Lowest Common Ancestor.cpp
+++ LCA - Lowest Common Ancestor.cpp
@@ -1,93 +1,101 @@
 /*
-Question: LCA - Lowest Common Ancestor 
-Link: https://www.codechef.com/problems/TALCA
-Algorithm: Lowest Common Ancestor 
-Created: 2017-08-10 14:22 IST 
+Question: LCA - Lowest Common Ancestor
+Link: http://www.spoj.com/problems/LCA/
+Algorithm: LCA.
+Created: 2018-01-24 01:26 IST 
+By: anudeex_cr7
 */
 
 #include <bits/stdc++.h>
 
-typedef long long ll;
+using namespace std;
 
-#define max_n (int)1005
-#define max_log (int)11
+typedef long long ll;
 
 #define pb push_back
 
-using namespace std;
+const ll maxN = 1005;
+const ll maxlog = 15;
 
-vector<int> graph[max_n];
+vector <ll> graph[maxN];
 
-int par[max_n][max_log],h[max_n];
+ll par[maxN][maxlog], h[maxN];
 
-void dfs(int curr,int prev){
-	par[curr][0]=prev;
-	if(prev!=-1){
-		h[curr]=h[prev]+1;
+void dfs(ll curr, ll prev) {
+	ll i, j, k;
+	par[curr][0] = prev;
+	if(prev != -1) {
+		h[curr] = h[prev] + 1;
 	}
-	int i,j,k;
-	for(i=1;i<max_log;i++){
-		if(par[curr][i-1]!=-1){
-			par[curr][i]=par[par[curr][i-1]][i-1];
+	for(i = 1; i < maxlog; i++) {
+		if(par[curr][i - 1] != -1) {
+			par[curr][i] = par[par[curr][i -1]][i - 1];
+		}
+		else {
+			break;
 		}
 	}
-	for(auto it:graph[curr]){
-		if(it!=prev){
-			dfs(it,curr);
+	for(auto it : graph[curr]) {
+		if(it != prev) {
+			dfs(it, curr);
 		}
 	}
 }
 
-int lca(int u,int v){
-	int i,j,k;
-	if(h[v]>h[u]){
-		swap(u,v);
+ll lca(ll u, ll v) {
+	ll i, j, k;
+	if(h[u] > h[v]) {
+		swap(u, v);
 	}
-	for(i=max_log-1;i>=0;i--){
-		if(par[u][i]!=-1 and h[par[u][i]]>=h[v]){
-			u=par[u][i];
+	for(i = maxlog - 1; i >= 0; i-- ) {
+		if(par[v][i] != -1 and h[par[v][i]] >= h[u]) {
+			v = par[v][i];
 		}
 	}
-	if(u==v){
+	if(v == u) {
 		return u;
 	}
-	for(i=max_log-1;i>=0;i--){
-		if(par[u][i]!=par[v][i]){
-			u=par[u][i];
-			v=par[v][i];
+	for(i = maxlog - 1; i >= 0; i--) {
+		if(par[v][i] != par[u][i]) {
+			u = par[u][i];
+			v = par[v][i];
 		}
 	}
 	return par[u][0];
 }
 
-int main(){
-	int t,i,j,k;
-	scanf("%d",&t);
-	for(k=1;k<=t;k++){
-		int n;
-		scanf("%d",&n);
-		for(i=1;i<=n;i++){
+int main() {
+	ll T, t = 1;
+	scanf("%lld", &T);
+	while(T--) {
+		ll N, M, i, j, k;
+		scanf("%lld", &N);
+		for(i = 1; i <= N; i++) {
 			graph[i].clear();
+			for(j = 0; j < maxlog; j++) {
+				par[i][j] = -1;
+			}
+			h[i] = 0;
 		}
-		for(i=1;i<=n;i++){
-			int sz;
-			scanf("%d",&sz);
-			while(sz--){
-				scanf("%d",&j);
-				graph[i].pb(j);
-				graph[j].pb(i);
+		for(i = 1; i <= N; i++) {
+			scanf("%lld", &M);
+			for(j = 1; j <= M; j++) {
+				ll a;
+				scanf("%lld", &a);
+				graph[a].pb(i);
+				graph[i].pb(a);
 			}
 		}
-		memset(par,-1,sizeof par);
-		dfs(1,-1);
-		int q;
-		scanf("%d",&q);
-		printf("Case %d:\n",k);
-		while(q--){
-			int a,b;
-			scanf("%d%d",&a,&b);
-			printf("%d\n",lca(a,b));
+		dfs(1, -1);
+		ll Q;
+		scanf("%lld", &Q);
+		printf("Case %lld:\n", t);
+		while(Q--) {
+			ll u, v;
+			scanf("%lld %lld", &u, &v);
+			printf("%lld\n", lca(u, v));
 		}
+		t++;
 	}
 	return 0;
 }
